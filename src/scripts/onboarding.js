9f19da13eddcda6b04467659f55955710af99b49
rahoc369782 <Rahul.Darekar@bom.tv18.com>
2023-03-24 22:03:04 +0530

    Inprogress dev authorization

--- src/scripts/onboarding.js
+++ src/scripts/onboarding.js
@@ -1,24 +1,105 @@
 'use strict';
+
+// On Domcontent loaded first we will check for session
+// if session for current_block is available then we will open that respetive block
+// else we will open default block
+// after fetching from 
+
+function patterns() {
+    return {
+        email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+    }
+}
 let allowedPagesForLoginOpt = {
     userid: true,
     otp: true,
 }
-function verifyOtp(event, flow) {
-    document.cookie = "current_step=password"
-    sessionStorage.setItem("last_step", "password")
-    openBlock(event, flow)
+
+async function call_api() {
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Done")
+        }, 2000)
+    })
 }
 
-// setCockie for registration tracking purpose
-function setCockie() {
-    // document.cookie = "last_step= password; path=/"
-    // document.cookie = "current_step=userid"
+function apply_keypress_event() {
+    document.querySelectorAll("[common-input]").forEach(input => {
+        input.addEventListener("keypress", (e) => {
+            let cur_btn = document.querySelector(`[${input.getAttribute("btn")}]`)
+            if (e.keyCode === 13) {
+                if (cur_btn) {
+                    cur_btn.innerHTML = '<div class="dot-spinner"><div></div><div></div><div></div><div></div></div>'
+                    cur_btn.click()
+                }
+            }
+        })
+    })
+
+
 }
 
-window.addEventListener("load", () => {
-    setCockie();
-    checkCockie()
+async function verifyOtp(event, flow) {
+    sessionStorage.setItem("rg_status", "password")
+    await call_api()
+    openrgBlock(event, flow)
+}
+
+async function setPassword(event, flow) {
+    sessionStorage.setItem("rg_status", "username")
+    await call_api()
+    openrgBlock(event, flow)
+}
+
+async function setUsername(event, flow) {
+    sessionStorage.setItem("rg_status", "personal")
+    sessionStorage.setItem("_username", "personal")
+    await call_api()
+    openrgBlock(event, flow)
+}
+
+async function setPersonalinfo(event, flow) {
+    sessionStorage.setItem("rg_status", "rg-done")
+    await call_api()
+    openrgBlock(event, flow)
+}
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    // Check for session storage
+    let currentStep = sessionStorage.getItem("rg_status");
+    let currentForm = sessionStorage.getItem("current_form");
+    if (currentForm) {
+        // document.querySelectorAll(".wrapper_form_active").forEach(item => {
+        //     item.classList.remove("wrapper_form_active")
+        // })
+        let enabledForm = document.querySelector(`[onboarding-${currentForm}-form-container]`)
+        if (enabledForm) {
+            enabledForm.classList.add("wrapper_form_active")
+        }
+    } else {
+        let enabledForm = document.querySelector(`[
+            onboarding-login-form-container]`)
+        if (enabledForm) {
+            enabledForm.classList.add("wrapper_form_active")
+        }
+    }
+    if (currentStep) {
+        document.querySelectorAll(".active_rg_inputblock").forEach(item => {
+            item.classList.remove("active_rg_inputblock")
+        })
+        // Enabled previous block in which user drop
+        let enabledBlock = document.querySelector(`[onb-rg-${currentStep}-block]`)
+        if (enabledBlock) {
+            enabledBlock.classList.add("active_rg_inputblock")
+        }
+    }
+    apply_keypress_event()
 })
+// window.addEventListener("load", () => {
+//     console.log("loaded", content - Date.now())
+//     setCockie();
+//     checkCockie()
+// })
 
 function enableRequiredBlock(track) {
     // Enable required block
@@ -26,7 +107,7 @@ function enableRequiredBlock(track) {
     if (!track) {
         document.querySelector('[onb-rg-userid-block]').classList.add("active_inputblock")
     } else {
-        let enabledBlock = document.querySelector(`[onb-rg-${track.current_step}-block]`)
+        let enabledBlock = document.querySelector(`[onb-rg-${track.rg_status}-block]`)
         if (enabledBlock) {
             enabledBlock.classList.add("active_inputblock")
         }
@@ -53,66 +134,85 @@ function checkCockie() {
         }, {});
     return enableRequiredBlock(c_obj)
 }
-function sendOtp(event, flow) {
+async function sendOtp(event, flow) {
+    console.log("ijfei")
     let userId = document.querySelector('#onb_email_input')
+    console.log(userId.value)
+    if (!patterns().email.test(userId.value)) {
+        event.target.classList.toggle("wrong_input_highlight")
+        event.target.innerHTML = "Send OTP"
+        return
+    }
     let userIdElement = document.querySelector('[user-unique-id]');
     if (userIdElement) {
         userIdElement.innerText = userId.value
     }
-    openBlock(event, flow)
+    await call_api()
+    openrgBlock(event, flow)
 }
 
-function openBlock(event, flow) {
+function openrgBlock(event, flow) {
     let current_input_block = event.target.closest(".input_group_block");
     if (!current_input_block) {
         return
     }
-
     if (current_input_block) {
-        current_input_block.classList.remove("active_inputblock")
+        current_input_block.classList.remove("active_rg_inputblock")
     }
     if (flow === 'n') {
         if (current_input_block.nextElementSibling) {
-            current_input_block.nextElementSibling.classList.add("active_inputblock")
-            let loginOpt = document.getElementById("onb_login_opt");
-            if (current_input_block.nextElementSibling.attributes.is_login_allowed) {
-                loginOpt.style.display = 'block'
-            } else {
-                loginOpt.style.display = 'none'
-            }
+            current_input_block.nextElementSibling.classList.add("active_rg_inputblock")
+            current_input_block.nextElementSibling.children[1].children[0].focus()
         }
     } else if (flow === "p") {
         if (current_input_block.previousElementSibling) {
-            current_input_block.previousElementSibling.classList.add("active_inputblock")
-        }
-        let loginOpt = document.getElementById("onb_login_opt");
-        if (current_input_block.previousElementSibling.attributes.is_login_allowed) {
-            loginOpt.style.display = 'block'
-        } else {
-            loginOpt.style.display = 'none'
+            current_input_block.previousElementSibling.classList.add("active_rg_inputblock")
+            current_input_block.previousElementSibling.children[1].children[0].focus()
         }
     }
+}
 
-    // document.querySelectorAll(".input_group_block").forEach(block => {
-    //     block.classList.remove("active_inputblock")
-    // })
-    // document.querySelector(selector).classList.add("active_inputblock")
+function openlgBlock(event, flow) {
+    let current_input_block = event.target.closest(".input_group_block");
+    if (!current_input_block) {
+        return
+    }
+    if (current_input_block) {
+        current_input_block.classList.remove("active_lg_inputblock")
+    }
+    if (flow === 'n') {
+        if (current_input_block.nextElementSibling) {
+            current_input_block.nextElementSibling.classList.add("active_lg_inputblock")
+            current_input_block.nextElementSibling.children[1].children[0].focus()
+
+        }
+    } else if (flow === "p") {
+        if (current_input_block.previousElementSibling) {
+            current_input_block.previousElementSibling.classList.add("active_lg_inputblock")
+            current_input_block.previousElementSibling.children[1].children[0].focus()
+        }
+
+    }
 
 }
 
-function openFormWrapper(event, flow) {
-    let current_wrapper_block = event.target.closest("[onboarding-form-container]");
-
-    current_wrapper_block.classList.remove("wrapper_form_active")
-    if (flow === 'n') {
-        console.log(current_wrapper_block.nextElementSibling)
-
-        if (current_wrapper_block.nextElementSibling) {
-            current_wrapper_block.nextElementSibling.classList.add("wrapper_form_active")
-        }
-    } else if (flow === "p") {
-        if (current_wrapper_block.previousElementSibling) {
-            current_wrapper_block.previousElementSibling.classList.add("wrapper_form_active")
-        }
+function openFormWrapper(close, open) {
+    let current_wrapper_block = document.querySelector(`[onboarding-${close}-form-container]`)
+    let next_wrapper_block = document.querySelector(`[onboarding-${open}-form-container]`)
+    if (!current_wrapper_block) {
+        return;
     }
+    current_wrapper_block.classList.remove("wrapper_form_active")
+    next_wrapper_block.classList.add("wrapper_form_active")
+    sessionStorage.setItem("current_form", open)
+    // if (flow === 'n') {
+    //     if (current_wrapper_block.nextElementSibling) {
+    //         current_wrapper_block.nextElementSibling.classList.add("wrapper_form_active")
+    //         sessionStorage.setItem("current_form",)
+    //     }
+    // } else if (flow === "p") {
+    //     if (current_wrapper_block.previousElementSibling) {
+    //         current_wrapper_block.previousElementSibling.classList.add("wrapper_form_active")
+    //     }
+    // }
 }
